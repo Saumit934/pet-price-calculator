3eddf364c563ab0b82fe9f5067331a1a0b3a3a62
Saumit Sujit Mandal <24051205@kiit.ac.in>
2026-03-27 23:52:12 +0530

    Add marketData.ts to Pet Price Calculator project

---
+++ src/engines/marketData.ts
@@ -0,0 +1,11 @@
+// Market Data
+
+// Sample market data for pets.
+const marketData = {
+    dogs: { averagePrice: 500 },
+    cats: { averagePrice: 300 },
+    birds: { averagePrice: 100 },
+    smallPets: { averagePrice: 150 }
+};
+
+export default marketData;
